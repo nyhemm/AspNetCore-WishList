baf7339d1957ef7e23dafb89139beb01a9738568
Nyalls Hemingway <nyalls.hemingway@scot.gov>
2020-05-21 11:10:33 +0000

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -39,7 +39,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
-            var item = _context.Items.Find(id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
